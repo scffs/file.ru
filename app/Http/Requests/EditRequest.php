e58fe73a80a68cbf54781963673b026ea1c18d30
scffs <khidey@inbox.ru>
2024-03-15 16:48:19 +0700

    add new requests

---
+++ app/Http/Requests/EditRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests;
+
+//use App\Models\File;
+
+class EditRequest extends ApiRequest
+{
+  /** Валидируем входящие данные */
+  public function rules(): array
+  {
+    return [
+      'name' => 'required|string',
+    ];
+  }
+}
---
+++ app/Http/Requests/RightAddRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Requests;
+
+class RightAddRequest extends ApiRequest
+{
+  public function rules(): array
+  {
+    return [
+      'email' => 'required|email|exists:users,email',
+    ];
+  }
+}
